8550489ba450fc1775aad88aba102b1164a0ced1
eberegit <samsoneberenjoku@gmail.com>
2022-04-29 17:58:20 +0100

    Bets-and-Wagers done

--- index.mjs
+++ index.mjs
@@ -5,12 +5,24 @@ const stdlib = loadStdlib();
 const startingBalance = stdlib.parseCurrency(100);
 
 // create account balace for Alice and Bob
-const AliceBalance = await stdlib.newTestAccount(startingBalance);
-const BobBalance = await stdlib.newTestAccount(startingBalance);
+const AliceAccount = await stdlib.newTestAccount(startingBalance);
+const BobAccount = await stdlib.newTestAccount(startingBalance);
+
+// format currency function
+// formats currency to 4-decimal place
+const currencyFormatter = (fund) => stdlib.formatCurrency(fund, 4);
+
+// get balance function
+const getBalance = async (person) =>
+  currencyFormatter(await stdlib.balanceOf(person));
+
+// get the balances before starting the agreement
+const AliceBalanceBefore = await getBalance(AliceAccount);
+const BobBalanceBefore = await getBalance(BobAccount);
 
 // create contracts for Alice and Bob
-const AliceContract = AliceBalance.contract(backend);
-const BobContract = BobBalance.contract(backend, AliceContract.getInfo());
+const AliceContract = AliceAccount.contract(backend);
+const BobContract = BobAccount.contract(backend, AliceContract.getInfo());
 
 // define the choices
 const CHOICE = ["Leave", "Stay"];
@@ -41,10 +53,28 @@ await Promise.all([
   // Alice
   AliceContract.p.Alice({
     ...Partaker("Alice"),
+    // decides a deposit to make
+    deposit: stdlib.parseCurrency(15),
   }),
 
   // Bob
   BobContract.p.Bob({
     ...Partaker("Bob"),
+    // accepts to make the same deposit
+    acceptDeposit: (amount) => {
+      console.log(
+        `Bob accepts to also deposit ${currencyFormatter(
+          amount
+        )} as proposed by Alice`
+      );
+    },
   }),
 ]);
+
+// get the balances before starting the agreement
+const AliceBalanceAfter = await getBalance(AliceAccount);
+const BobBalanceAfter = await getBalance(BobAccount);
+
+// display final each person's account balance
+console.log(`Alice went from ${AliceBalanceBefore} to ${AliceBalanceAfter}`);
+console.log(`Bob went from ${BobBalanceBefore} to ${BobBalanceAfter}`);
--- index.rsh
+++ index.rsh
@@ -10,10 +10,14 @@ export const main = Reach.App(() => {
   const Alice = Participant("Alice", {
     // extend or take the Partaker properties
     ...Partaker,
+    // define the deposit
+    deposit: UInt,
   });
   const Bob = Participant("Bob", {
     // extend or take the Partaker properties
     ...Partaker,
+    // define the acceptDeposit function
+    acceptDeposit: Fun([UInt], Null),
   });
 
   // start the program
@@ -22,17 +26,20 @@ export const main = Reach.App(() => {
   // Alice local step
   // Alice interact with the frontend to make a choice and commit
   Alice.only(() => {
+    const deposit = declassify(interact.deposit);
     const AliceChoice = declassify(interact.getChoice());
   });
-  Alice.publish(AliceChoice);
+  // makes her hand public and pays in her deposit
+  Alice.publish(deposit, AliceChoice).pay(deposit);
   commit();
 
   // Bob local step
   // Bob interact with the frontend to make a choice
   Bob.only(() => {
+    interact.acceptDeposit(deposit);
     const BobChoice = declassify(interact.getChoice());
   });
-  Bob.publish(BobChoice);
+  Bob.publish(BobChoice).pay(deposit);
 
   // compute the result depending on their choices
   const outcome =
@@ -43,6 +50,30 @@ export const main = Reach.App(() => {
       : AliceChoice === 1 && BobChoice === 0
       ? 2
       : 3;
+
+  // transfer to the winner
+  // No one is supposed to get a dim but since we can't leave money in the contract, we do a refund
+  if (outcome === 1) {
+    transfer(1 * deposit).to(Alice);
+    transfer(1 * deposit).to(Bob);
+  }
+
+  //   Bob takes all
+  if (outcome === 0) {
+    transfer(2 * deposit).to(Bob);
+  }
+
+  //   Alice takes all
+  if (outcome === 2) {
+    transfer(2 * deposit).to(Alice);
+  }
+
+  //   each one gets a refund
+  if (outcome === 3) {
+    transfer(1 * deposit).to(Alice);
+    transfer(1 * deposit).to(Bob);
+  }
+
   commit();
 
   // pass the outcome for each to see the result
