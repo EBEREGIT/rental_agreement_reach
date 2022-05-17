a9c5d90e69501846352026b9ffa65551593091d2
eberegit <samsoneberenjoku@gmail.com>
2022-05-17 13:16:00 +0100

    Interaction and Independence done

--- index.mjs
+++ index.mjs
@@ -1,31 +1,126 @@
-import { loadStdlib } from "@reach-sh/stdlib";
+import { loadStdlib, ask } from "@reach-sh/stdlib";
 import * as backend from "./build/index.main.mjs";
 const stdlib = loadStdlib();
 
-const startingBalance = stdlib.parseCurrency(100);
+// check if the player is Alice or Bob
+const isAlice = await ask.ask(`Are you Alice?`, ask.yesno);
+const person = isAlice ? "Alice" : "Bob";
 
-// create account balance for Alice and Bob
-const AliceAccount = await stdlib.newTestAccount(startingBalance);
-const BobAccount = await stdlib.newTestAccount(startingBalance);
+console.log(`You are entering this agreement as ${person}`);
+
+// create account
+let userAccount = null;
+
+// ask to create a new account
+const createAccount = await ask.ask(
+  `Would you like to create a new account?`,
+  ask.yesno
+);
+
+// create a new account if createAccount is true else just join the contract with your password
+if (createAccount) {
+  userAccount = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+} else {
+  const accountPassword = await ask.ask(
+    `What is the account password?`,
+    (password) => password
+  );
+
+  userAccount = await stdlib.newAccountFromSecret(accountPassword);
+}
+
+// create contract
+let transactionContract = null;
+if (isAlice) {
+  transactionContract = userAccount.contract(backend);
+  transactionContract.getInfo().then((contractInfo) => {
+    console.log(
+      `Your contract has been deployed as ${JSON.stringify(contractInfo)}`
+    );
+  });
+} else {
+  const contractInfo = await ask.ask(
+    `Please enter your contract details:`,
+    JSON.parse
+  );
+  transactionContract = userAccount.contract(backend, contractInfo);
+}
 
 // format currency function
 // formats currency to 4-decimal place
 const currencyFormatter = (fund) => stdlib.formatCurrency(fund, 4);
 
 // get balance function
-const getBalance = async (person) =>
-  currencyFormatter(await stdlib.balanceOf(person));
+const getBalance = async () =>
+  currencyFormatter(await stdlib.balanceOf(userAccount));
 
-// get the balances before starting the agreement
-const AliceBalanceBefore = await getBalance(AliceAccount);
-const BobBalanceBefore = await getBalance(BobAccount);
+// get user's previous balance
+const balanceBefore = await getBalance();
+console.log(`Your Balance is ${balanceBefore}`);
 
-// create contracts for Alice and Bob
-const AliceContract = AliceAccount.contract(backend);
-const BobContract = BobAccount.contract(backend, AliceContract.getInfo());
+// start participant interaction interface
+const interact = { ...stdlib.hasRandom };
+
+// define the timeout function
+interact.participantTimeout = () => {
+  console.log(`There was a timeout`);
+  process.exit(1);
+};
+
+// deposit and deadline (timeUp)
+if (isAlice) {
+  // setup deposit
+  const amount = await ask.ask(
+    `How much do you want to deposit?`,
+    stdlib.parseCurrency
+  );
+  interact.deposit = amount;
+  interact.timeUp = { ETH: 100, ALGO: 100, CFX: 1000 }[stdlib.connector];
+} else {
+  // setup accept deposit function
+  interact.acceptDeposit = async (amount) => {
+    const accepted = await ask.ask(
+      `Do you accept to make the same deposit of ${currencyFormatter(
+        amount
+      )} as Alice?`,
+      ask.yesno
+    );
+
+    // terminate if terms are not accepted
+    if (!accepted) {
+      process.exit(0);
+    }
+  };
+}
 
 // define the choices
 const CHOICE = ["Leave", "Stay"];
+const CHOICES = {
+  leave: 0,
+  L: 0,
+  l: 0,
+  LEAVE: 0,
+  stay: 1,
+  S: 1,
+  s: 1,
+  STAY: 1,
+};
+
+// get choice function
+interact.getChoice = async () => {
+  const choice = await ask.ask(`Do you want to STAY or LEAVE?`, (result) => {
+    const resultingChoice = CHOICES[result];
+    if (resultingChoice === undefined) {
+      throw Error(`${resultingChoice} isn't an option!`);
+    }
+    return resultingChoice;
+  });
+
+  console.log(`You want to ${CHOICE[choice]}`);
+  return choice;
+};
+
+// define outcome
 const RESULT = [
   "Bob Takes All the money",
   "No one takes a dime",
@@ -33,65 +128,17 @@ const RESULT = [
   "Each Person's money is refunded",
 ];
 
-// Define the Partaker
-const Partaker = (person) => ({
-  ...stdlib.hasRandom,
+interact.seeResult = async (result) => {
+  console.log(`${RESULT[result]}`);
+};
 
-  getChoice: async () => {
-    // make a random choice by picking a number between 1 & 2
-    const choice = Math.floor(Math.random() * 2);
-    console.log(`${person} wants to ${CHOICE[choice]}`);
-    
-    // set a delay to force a timeout sometimes
-    if ( Math.random() <= 0.01 ) {
-      for ( let i = 0; i < 10; i++ ) {
-        console.log(`${person} takes their sweet time sending it back...`);
-        await stdlib.wait(1);
-      }
-    }
-    
-    return choice;
-  },
+const individual = isAlice
+  ? transactionContract.p.Alice
+  : transactionContract.p.Bob;
 
-  seeResult: (result) => {
-    console.log(`${RESULT[result]}`);
-  },
+await individual(interact);
 
-  participantTimeout: () => {
-    console.log(`${person} observed a timeout`);
-  }
-});
+const balanceAfter = await getBalance();
+console.log(`Your balance is now ${balanceAfter}`);
 
-// initialize the backend
-// execute the steps Alice and Bob takes in this program
-await Promise.all([
-  // Alice
-  AliceContract.p.Alice({
-    ...Partaker("Alice"),
-    // decides a deposit to make
-    deposit: stdlib.parseCurrency(15),
-    // decide how long before time out
-    timeUp: 10,
-  }),
-
-  // Bob
-  BobContract.p.Bob({
-    ...Partaker("Bob"),
-    // accepts to make the same deposit
-    acceptDeposit: (amount) => {
-        console.log(
-          `Bob accepts to also deposit ${currencyFormatter(
-            amount
-          )} as proposed by Alice`
-        );
-    },
-  }),
-]);
-
-// get the balances before starting the agreement
-const AliceBalanceAfter = await getBalance(AliceAccount);
-const BobBalanceAfter = await getBalance(BobAccount);
-
-// display final each person's account balance
-console.log(`Alice went from ${AliceBalanceBefore} to ${AliceBalanceAfter}`);
-console.log(`Bob went from ${BobBalanceBefore} to ${BobBalanceAfter}`);
+ask.done();
